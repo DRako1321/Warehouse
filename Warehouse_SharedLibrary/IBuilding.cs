a909a59acf1ba0a9e6b939daa733b845d48a799f
drako <drakoczy3@gmail.com>
2025-04-26 14:39:35 +0200

    interfacees frames done

--- Warehouse_SharedLibrary/IBuilding.cs
+++ Warehouse_SharedLibrary/IBuilding.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Warehouse_SharedLibrary
+namespace Warehouse_SharedLibrary;
+
+internal interface IBuilding
 {
-    internal interface IBuilding
-    {
-    }
 }
